8e5fd72321646bac3b525bd84721d60c3a88ff47
ItsMeTomB <tomball.me@icloud.com>
2015-10-05 22:23:33 +0100

    added database setup

---
+++ db/db_setup.py
@@ -0,0 +1,62 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String,\
+	create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Restaurant(Base):
+	__tablename__ = 'restaurant'
+
+	id = Column(
+		Integer, primary_key = True)
+
+	name = Column(
+		String(80), nullable = False)
+
+	image = Column(
+		String(80))
+
+class MenuItem(Base):
+	__tablename__ = 'menu_item'
+
+	id = Column(
+		Integer, primary_key = True)
+
+	name = Column(
+		String(80), nullable = False)
+
+	course = Column(
+		String(250))
+
+	description = Column(
+		String(250))
+
+	price = Column(
+		String(8))
+
+	image = Column(
+		String(80))
+
+	restaurant_id = Column(
+		Integer, ForeignKey('restaurant.id'))
+
+	restaurant = relationship(Restaurant)
+
+	@property
+	def serialize(self):
+		return {
+			'id' : self.id,
+			'name' : self.name,
+			'course' : self.course,
+			'description' : self.description,
+			'price' : self.price,
+			'image' : self.image,
+		}
+
+engine = create_engine(
+	'sqlite:///restaurant.db')
+
+Base.metadata.create_all(engine)
+
