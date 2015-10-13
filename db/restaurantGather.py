8a2d19742f4a3df5e56a763a91f859e8f06cb5cc
ItsMeTomB <tomball.me@icloud.com>
2015-10-13 20:59:41 +0100

    Updated getImage to save images as id name

--- db/restaurantGather.py
+++ db/restaurantGather.py
@@ -11,7 +11,7 @@ class GetPlaces:
     """ Takes various info and returns results from the google places api. """
     def __init__(self, token):
         self.url_base = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" # ? indicates start of variables
-        self.location = "location=" + raw_input("location in 'lon,lat'(decimal) format > ") # location does not need & as it begins the variable string
+        self.location = "location=" + "51.5,-0.116"#raw_input("location in 'lon,lat'(decimal) format > ") # location does not need & as it begins the variable string
         self.radius = "&radius=" + raw_input("radius in meters (up to 50000) > ")
         self.types = "&types=" + raw_input("type > ")
         self.name = "&name=" + raw_input("keyword > ")
@@ -35,16 +35,21 @@ class GetPlaces:
         return jDict
 
     def getImage(self):
+        n = 0
         jDict = self.jsonRequest()
-        photos = jDict[1]['photos'][0]["photo_reference"]
-        rstring = "https://maps.googleapis.com/maps/api/place/photo?" + "maxwidth=400" + "&photoreference=" + photos + self.key
-        photo = requests.get(rstring, stream=True)
-        if photo.status_code == 200:
-            with open("image.png", 'wb') as filewrite:
-                for chunk in photo:
-                    filewrite.write(chunk)
+        while n != len(jDict):
+            try:
+                photos = jDict[n]['photos'][0]["photo_reference"]
+                rstring = "https://maps.googleapis.com/maps/api/place/photo?" + "maxwidth=400" + "&photoreference=" + photos + self.key
+                photo = requests.get(rstring, stream=True)
+                if photo.status_code == 200:
+                    with open("image" + str(jDict[n]['id']) + ".png", 'wb') as filewrite:
+                        for chunk in photo:
+                            filewrite.write(chunk)
+            except:                
+                print "No Image"
+            n += 1
         print rstring
-        print photo_url
 mysearch = GetPlaces(ACCESS_TOKEN) # to use your own api token replace ACCESS_TOKEN with your own in a string
 jDict = mysearch.jsonRequest()
 
