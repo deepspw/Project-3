d4b41b4cfd7c788527b33fc13b662640cd47669b
Tom Panda <tomball.me@icloud.com>
2015-11-18 22:30:55 +0000

    menuGather reworked

--- db/menuGather.py
+++ db/menuGather.py
@@ -11,9 +11,20 @@ resta = session.query(Restaurant).all()
 
 def itemset(restaID):
     MenuItem1 = [MenuItem(name="Fish Cake", course = 'Entree', description='Fish cake patty with tartar sauce', price = "$6.00", restaurant = restaID ),
-        MenuItem(name="Pizza", course = 'Entree', description='Fish cake patty with tartar sauce', price = "$6.00", restaurant = restaID )
+        MenuItem(name="Potted Shrimp", course = 'Appertiser', description='Brown shrimp with nutmeg butter', price = "$7.20", restaurant = restaID ),
+        MenuItem(name="Dough Balls", course = 'Appertiser', description='Warm dough balls, with garlic and parsley dip', price = "$4.95", restaurant = restaID ),
+        MenuItem(name="Garlic Bread", course = 'Appertiser', description='Baked garlic bread', price = "$4.75", restaurant = restaID ),
+        MenuItem(name="Caesar Salad", course = 'Appertiser', description='Baby gem letace with Caesar dressing', price = "$5.45", restaurant = restaID ),
+        MenuItem(name="Calamari", course = 'Appertiser', description='Lightly fried calamari with selection of dips', price = "$6.45", restaurant = restaID ),
+        MenuItem(name="Mac N Cheese", course = 'Entree', description='Classic mac n cheese with pulled pork and BBQ sauce', price = "$11.95", restaurant = restaID ),
+        MenuItem(name="Margherita Pizza", course = 'Entree', description='Tomato and mozzarella on homebaked pizza base', price = "$8.75", restaurant = restaID ),
+        MenuItem(name="Veggie Burger", course = 'Entree', description='Toasted brioche bun with chickpea patty', price = "$5.20", restaurant = restaID ),
+        MenuItem(name="Beef Burger", course = 'Entree', description='Prime beef with onions, tomato, pickles and chillies', price = "$11.20", restaurant = restaID ),
+        MenuItem(name="Fish Stew", course = 'Entree', description='Fish cooked in rich Fiano white wine sauce', price = "$12.85", restaurant = restaID ),
+        MenuItem(name="Ginger Mojito", course = 'Beverage', description='Mint, lime, sugar & ginger bear', price = "$3.20", restaurant = restaID ),
+        MenuItem(name="Diet Coke", course = 'Beverage', description='Diet cola', price = "$2.40", restaurant = restaID )        
         ]
-    x = random.randint(0, 1)
+    x = random.randint(0, (len(MenuItem1)-1))
     return MenuItem1[x]
 
 for e in resta:
