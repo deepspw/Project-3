5612805cb3de63dfa61f43df93b1836e4c15d2cf
Tom Panda <tomball.me@icloud.com>
2015-12-14 16:54:36 +0000

    Added gAPI template file

---
+++ gAPI.py
@@ -0,0 +1,7 @@
+
+# Will be listed under API keys, used for google places api in restaurantGather
+ACCESS_TOKEN = "Your token here"
+
+# to download your client_secret look under OAuth 2.0 client ID's in your google API Manager, used for login functionality
+# Be sure to authorise javascript origins http//localhost:5000 as well as redirects localhost:5000/*
+# once downloaded rename to client_secrets
